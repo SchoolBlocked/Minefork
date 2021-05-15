2bf7d14e1c1d71bbeb2edb8408edc068c04c3c44
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2021-05-15 04:14:34 -0700

    PWA Functionality
    Added basic PWA functionality! The site will now work offline, and it will have dedicated favicon and installation icons.

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.Minefork = {
+  version: 1.00
+};
+self.addEventListener("activate",event => {
+  event.waitUntil(caches.keys().then(versions => Promise.all(versions.map(cache => {
+    if (cache != Minefork.version) return caches.delete(cache);
+  }))));
+  event.waitUntil(clients.claim());
+});
+self.addEventListener("fetch",event => {
+  event.respondWith(caches.match(event.request).then(response => {
+    return response || fetch(event.request).then(async response => {
+      caches.open(Minefork.version).then(cache => cache.put(event.request,response));
+      return response.clone();
+    });
+  }));
+});
